2b277a0ff57dc5f7bf9eb1fcc2fea30411cc3ee2
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-30 16:16:46 +0100

    Initial Commit

---
+++ BDays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BDays
+//
+//  Created by Sukumar Anup Sukumaran on 30/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
